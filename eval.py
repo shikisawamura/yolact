8a3cee8e5df5299e56604850da0c66ebba9d7fe4
shikisawamura <shikisawamura@gmail.com>
2020-10-21 18:44:05 +0900

    Update eval.py

--- eval.py
+++ eval.py
@@ -189,9 +189,9 @@ def prep_display(dets_out, img, h, w, undo_transform=True, class_color=False, ma
     if args.display_masks and cfg.eval_mask_branch and num_dets_to_consider > 0:
         # After this, mask is of size [num_dets, h, w, 1]
         masks = masks[:num_dets_to_consider, :, :, None]
-        img_gpu *= (masks.sum(dim=0) >= 1).float().expand(-1, -1, 3)
+        img_gpu = (masks.sum(dim=0) >= 1).float().expand(-1, -1, 3).contiguous()
     else:
-        img_gpu *= 0
+        img_gpu *= 0.5
         
         # Prepare the RGB images for each mask given their color (size [num_dets, h, w, 1])
         colors = torch.cat([get_color(j, on_gpu=img_gpu.device.index).view(1, 1, 1, 3) for j in range(num_dets_to_consider)], dim=0)
