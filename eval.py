36683c9ccd5461856521deeae7df215731ae42de
shikisawamura <shikisawamura@gmail.com>
2020-10-27 12:13:19 +0900

    Update eval.py

--- eval.py
+++ eval.py
@@ -189,7 +189,8 @@ def prep_display(dets_out, img, h, w, undo_transform=True, class_color=False, ma
     if args.display_masks and cfg.eval_mask_branch and num_dets_to_consider > 0:
         # After this, mask is of size [num_dets, h, w, 1]
         masks = masks[:num_dets_to_consider, :, :, None]
-        img_gpu = (masks.sum(dim=0) >= 1).float().expand(-1, -1, 3).contiguous()
+        #img_gpu = (masks.sum(dim=0) >= 1).float().expand(-1, -1, 3).contiguous()
+        img_gpu = [1.0, 0.0, 0.0, 1.0]
     else:
         #img_gpu *= 0
         img_gpu = [0.0, 1.0, 0.0, 1.0]
