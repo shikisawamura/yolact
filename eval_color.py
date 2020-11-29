b03242261b82d2a3b581e3b1be28ffe5584e88ee
shikisawamura <shikisawamura@gmail.com>
2020-11-29 09:52:23 +0900

    Update eval_color.py

--- eval_color.py
+++ eval_color.py
@@ -194,9 +194,12 @@ def prep_display(dets_out, img, h, w, undo_transform=True, class_color=False, ma
         # After this, mask is of size [num_dets, h, w, 1]
         masks = masks[:num_dets_to_consider, :, :, None]
         
+        #add by me
+        mask_alpha=1.0
+        
         # Prepare the RGB images for each mask given their color (size [num_dets, h, w, 1])
         colors = torch.cat([get_color(j, on_gpu=img_gpu.device.index).view(1, 1, 1, 3) for j in range(num_dets_to_consider)], dim=0)
-        masks_color = masks.repeat(1, 1, 1, 3) * colors # * mask_alpha
+        masks_color = masks.repeat(1, 1, 1, 3) * colors  * mask_alpha
 
         # This is 1 everywhere except for 1-mask_alpha where the mask is
         inv_alph_masks = masks * (-mask_alpha) + 1
